4e5baf882da21427395bf6f07ee3fd381479e120
Thiago Rodrigues <thiago.nascimento@opah.com.br>
2025-10-17 21:07:24 -0300

    first commit

---
+++ src/main/java/com/tr_systems/CondNotifyApplication.java
@@ -0,0 +1,14 @@
+package com.tr_systems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondNotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondNotifyApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/tr_systems/CondNotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tr_systems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondNotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
